21a438a750224d09fc2332d2b771e83b99b6fdeb
Milka404 <Milka2158@gmail.com>
2018-02-15 15:17:27 +0700

    доска первая

---
+++ chessviz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"8 r n b d k b n r" << "\n";
+    cout <<"7 p p p p p p p p" << "\n";
+    cout <<"6" << endl;
+    cout <<"5" << endl;
+    cout <<"4" << endl;
+    cout <<"3" << endl;
+    cout <<"2 P P P P P P P P" << "\n";
+    cout <<"1 R N B Q K B N R" << "\n";
+    cout <<"  a b c d e f g h" <<"\n";
+
+  return 0;
+
+}
